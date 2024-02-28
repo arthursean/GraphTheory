471449ce1d16bfd31d496846568aa67537a31d6e
arthursean <113319924+arthursean@users.noreply.github.com>
2024-02-27 21:50:14 -0300

    padrao_do_projeto

--- dijsktra/dijkstra.cpp
+++ dijsktra/dijkstra.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
+#include <bits/stdc++.h>
 #define maximum INT_MAX
 using namespace std;
 
@@ -29,10 +26,51 @@ vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int s)
     }
     return dist;
 }
-int main()
+int main(int argc, char *argv[])
 {
-    int n, m, p, v1, v2, s;
-    cin >> n >> m;
+    string outFile = "";
+    string inputFile = "";
+    int n, m, p, v1, v2;
+    int s = 1;
+    for (int i = 1; i < argc; i++)
+    {
+        if (!strcmp(argv[i], "-h"))
+        {
+            cout << "Help:" << endl;
+            cout << "-h : mostra o help" << endl;
+            cout << "-o <arquivo> : redireciona a saida para o ‘‘arquivo’’" << endl;
+            cout << "-f <arquivo> : indica o ‘‘arquivo’’ que contém o grafo de entrada" << endl;
+            cout << "-i : vértice inicial" << endl;
+            return 0;
+        }
+        if (!strcmp(argv[i], "-o") && i < argc - 1)
+        {
+            outFile = argv[++i];
+            continue;
+        }
+        if (!strcmp(argv[i], "-i") && i < argc - 1)
+        {
+            s = atoi(argv[++i]);
+            continue;
+        }
+        if (!strcmp(argv[i], "-f") && i < argc - 1)
+        {
+            inputFile = argv[++i];
+            continue;
+        }
+    }
+    if (inputFile == "")
+    {
+        cout << "Nenhum arquivo de entrada escolhido. Use '-f' para escolher o arquivo" << endl;
+        return 0;
+    }
+    ifstream fp(inputFile);
+    if (!fp)
+    {
+        cout << "O arquivo não consegue ser aberto ou não existe" << endl;
+        return 0;
+    }
+    fp >> n >> m;
     n++;
     vector<vector<pair<int, int>>> adj(n);
     for (int i = 1; i < n; i++)
@@ -41,12 +79,33 @@ int main()
     }
     for (int i = 0; i < m; i++)
     {
-        cin >> v1 >> v2 >> p;
+        fp >> v1 >> v2 >> p;
         adj[v1].push_back(make_pair(v2, p));
         adj[v2].push_back(make_pair(v1, p));
     }
-    cin >> s;
-    vector<int> dist = dijkstra(adj, 1);
+    fp.close();
+    vector<int> dist = dijkstra(adj, s);
+    if (!(outFile == ""))
+    {
+        ofstream fp(outFile);
+        if (!fp)
+        {
+            cout << "Não foi possível abrir o arquivo de saída: " << outFile << endl;
+        }
+        else
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (dist[i] == maximum)
+                {
+                    dist[i] = -1;
+                }
+                fp << i << ":" << dist[i] << " ";
+            }
+            fp << endl;
+            fp.close();
+        }
+    }
     for (int i = 1; i < n; i++)
     {
         if (dist[i] == maximum)
